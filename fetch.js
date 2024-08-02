e41b9c10a5060c5fa28a0cc5638285fd22a47f16
Pir Shukarullah Shah Rashdi <prashdi@gulfnews.com>
2024-08-02 16:38:23 +0400

    fix fetch

--- fetch.js
+++ fetch.js
@@ -2,13 +2,20 @@ import { create } from "apisauce";
 
 let api;
 
-function MyPromise(response: { ok: boolean, data: {} }) {
+function createApiPromise(response) {
   return new Promise((resolve, reject) => {
     if (response.ok && response.data) {
       resolve(response.data);
+    } else {
+      reject(response.data || response.problem || "SOMETHING WENT WRONG");
     }
-    reject(response.data || response.problem || "SOMETHING WENT WRONG");
   });
+}
+
+function encodeParams(params) {
+  return Object.entries(params)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+    .join('&');
 }
 
 export function createApi(baseURL) {
@@ -21,28 +28,27 @@ export function createApi(baseURL) {
 }
 
 export async function get(url, params) {
-  let body = [];
-  Object.keys(params).forEach((key) => {
-    body.push(`${key}=${params[key]}`);
-  });
-  body = body.join("&");
+  if (!api) throw new Error('API not initialized. Call createApi first.');
+
+  const body = encodeParams(params);
   const response = await api.post(url, body);
+
   if (__DEV__) {
-        console.log("Piano SDK - get - response:", response);
+    console.log("Piano SDK - get - response:", response);
   }
-  return MyPromise(response);
+
+  return createApiPromise(response);
 }
 
 export async function post(url, params, body) {
-  let queryString = [];
-  Object.keys(params).forEach((key) => {
-    queryString.push(`${key}=${params[key]}`);
-  });
-  queryString = queryString.join("&");
+  if (!api) throw new Error('API not initialized. Call createApi first.');
+
+  const queryString = encodeParams(params);
   const response = await api.post(`${url}?${queryString}`, body, {
     headers: {
       "Content-Type": "application/json",
     },
   });
-  return MyPromise(response);
+
+  return createApiPromise(response);
 }
